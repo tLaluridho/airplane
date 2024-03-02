792742a0f38dd98c6bb607c328c47312286d1ae1
LaluRidho <ridhotaufik2003@gmail.com>
2024-03-02 07:20:57 +0700

    Membuat project airplane serta menambah assets dan membuat theme warna&font membuat halaman splash_page

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:airplane/ui/pages/splash_pages.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
