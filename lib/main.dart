6926877047d8893dd9b8d1dbadb83e59a2e09bf8
LaluRidho <ridhotaufik2003@gmail.com>
2024-03-02 17:14:00 +0700

    membuat route baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
-import 'package:airplane/ui/pages/splash_pages.dart';
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/sign_up.page.dart';
+import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/get-started': (context) => const GetStartedPage(),
+        '/sign_up': (context) => const SignUpPage(),
+      },
     );
   }
 }
